9bd5215312cc45246a7915179f4e5a1433df1ca2
Ali Almaamouri <alialm05@proton.me>
2024-05-16 17:38:28 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='./Clapper-icon.png' className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A Discord Bot for finding out upcoming Movies!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://discord.com/oauth2/authorize?client_id=1240749734680072253&permissions=52948357138512&scope=bot"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        <button className='btn btn-primary btn-lg active'>
+          Invite to your Server  
+          </button>
+        
+        
         </a>
       </header>
     </div>
